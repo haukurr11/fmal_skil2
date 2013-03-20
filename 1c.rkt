cc8753e942a72a31e5eb689cf82f52332b27df00
Haukur Rosinkranz <hauxir@gmail.com>
2013-03-20 02:09:44 +0000

    1c

---
+++ 1c.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 1c) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(define (delete lis k)
+    (cond
+      ((null? lis) '())
+      ((equal? (car lis) k) (cdr lis))
+      (else (cons (car lis) (delete (cdr lis) k)))
+      ) 
+) 
