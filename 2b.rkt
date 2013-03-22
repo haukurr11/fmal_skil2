e9da3ebd004fe326ec5204da36bd21cbcfbcb61a
pdarkness <kntur11@ru.is>
2013-03-22 23:53:04 +0000

    blalsdlasl

---
+++ 2b.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 2b) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(define (shorten f lis)
+  (if
+    (null? (cdr lis)) (car lis)
+  (f (car lis) (shorten f (cdr lis)))
+  )
+)
+
+(define (shorten_all f lists)
+  (if
+    (null? (cdr lists)) (shorten f (car lists))
+  (f (shorten f (car lists)) (shorten_all f (cdr lists)))
+  )
+)
