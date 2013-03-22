f439cedb995dd1d0799a579edff45e3daffd5921
pdarkness <kntur11@ru.is>
2013-03-22 15:34:59 +0000

    blalbalb

--- 2a.rkt
+++ 2a.rkt
@@ -2,15 +2,8 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 2a) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 (define (shorten f lis)
-  (cond
-    ((null? (cdr lis)) 
-       (if (eq? f *) (car lis) 
-       (f (car lis)))
-    )
-    (else 
-     (f (car lis) 
-     (shorten f (cdr lis))
-     )
-    )
+  (if
+    (null? (cdr lis)) (car lis)
+  (f (car lis) (shorten f (cdr lis)))
+  )
 )
-)  
